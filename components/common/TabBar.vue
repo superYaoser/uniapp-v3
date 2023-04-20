841cdcad9373e64a93872ee8a1399d7d20507665
Yaoser <yao@yao.cn>
2023-04-20 13:35:05 +0800

    做了底部的静态效果

---
+++ components/common/TabBar.vue
@@ -0,0 +1,74 @@
+<template>
+	<view id="TabBar">
+		<view class="tabbar">
+			<view><uni-icons type="home" size="30" color="#999797" @click="goHome()"></uni-icons><text>首页</text></view>
+			<view><uni-icons type="pyq" size="30" color="#999797"></uni-icons><text>动态</text></view>
+			<view><uni-icons type="plus-filled" size="45" color="#13dbf9"></uni-icons></view>
+			<view><uni-icons type="chat" size="30" color="#999797"></uni-icons><text>消息</text></view>
+			<view><uni-icons type="person" size="30" color="#999797"></uni-icons><text>我的</text></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "TabBar",
+    setup(){
+      let staticIconsColor = '#999797';
+      let activityIconsColor='#13dbf9';
+
+      //首页
+      const goHome = (e)=>{
+        console.log(e)
+      }
+      //动态
+      const goDynamic = ()=>{
+
+      }
+      //发布
+      const goPublish = ()=>{
+
+      }
+      //我的
+      const goMine = ()=>{
+
+      }
+
+      return{
+        staticIconsColor,
+        goHome,
+        goDynamic,
+        goPublish,
+        goMine
+      }
+    }
+	}
+</script>
+
+<style lang="less" scoped>
+	#TabBar {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		z-index: 999;
+		width: 100%;
+		height: 60px;
+		background-color: #f9f9f9;
+
+		.tabbar {
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+			width: 100%;
+			height: 100%;
+			view{
+				  display: flex;
+				  flex-direction: column;
+				  align-items: center;
+				  text{
+					    font-size: 12px;
+				  }
+			}
+		}
+	}
+</style>
---
+++ pages/home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<view id="Home">
+		<TabBar></TabBar>
+	</view>
+</template>
+
+<script>
+	import TabBar from "@/components/common/TabBar.vue"
+	export default {
+		components: {
+			TabBar
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
